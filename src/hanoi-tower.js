e346fae23781419bc2d6749cc311770dbaeffdb5
FeNixVoVa <FeNixVoVa@gmail.com>
2020-10-23 17:47:00 +0300

    feat complete Hanoi Tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  if ((!Number.isInteger(disksNumber)) || (!Number.isInteger(turnsSpeed))) return 'Wrong arguments';
+  let turns = ((2 ** disksNumber) - 1);
+  let speedPerSecond = (turnsSpeed / 3600);
+  let timeRequires = (turns / speedPerSecond);
+  let result = new Object();
+  result.turns = turns;
+  result.seconds = Math.floor(timeRequires);
+  return result;
 };
